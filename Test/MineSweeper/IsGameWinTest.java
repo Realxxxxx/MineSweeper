3f444b57218c588cafcab3f39e2323b67fb000b1
Catherine-YQ <1047918132@qq.com>
2020-04-11 22:01:54 +0800

    inputCo testcase add

--- Test/MineSweeper/IsGameWinTest.java
+++ Test/MineSweeper/IsGameWinTest.java
@@ -1,10 +1,10 @@
 package MineSweeper;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+//import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.jboss.shrinkwrap.api.ShrinkWrap;
+//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+//import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
